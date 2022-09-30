cbc4280a6880d1609406091eb143283cb3223a12
maroschnovak <114218595+maroschnovak@users.noreply.github.com>
2022-09-30 13:39:11 +0200

    Update Uebung02.js

--- Uebung02/Uebung02.js
+++ Uebung02/Uebung02.js
@@ -17,30 +17,9 @@ console.log(L)
 function wuerfeln() {
     var f = [1,2,3,4,5,6];
 
-    var choice = 6*Math.random();
-
-    var index = 0;
-
-    if (choice < 1) {
-        index = 0;
-    }
-    else if (choice < 2) {
-        index = 1;
-    }
-    else if (choice < 3) {
-        index = 2;
-    }
-    else if (choice < 4) {
-        index = 3;
-    }
-    else if (choice < 5) {
-        index = 4;
-    }
-    else if (choice < 6) {
-        index = 5;
-    }
-
-    console.log(f[index]);
+    var choice = f.length*Math.random();
+    var index = Math.floor(choice);
+    return f[index];
 }
 
-wuerfeln();
+console.log(wuerfeln());
